65b95034690414c563485afae67cf02a6540e13f
mmadreza <mohammd78t@gmail.com>
2021-11-18 19:49:47 +0330

    startapp

---
+++ Transportation/Transportation/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Transportation project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Transportation.settings')
+
+application = get_asgi_application()
