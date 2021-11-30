de44e7de336c39f8db9bcf609f254e49d70e66e1
mmadreza <mohammd78t@gmail.com>
2021-12-01 01:09:03 +0330

    init midel waybill

--- Transportation/waybill/models.py
+++ Transportation/waybill/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from drivers.models import DriverMore
+from customer.models import CustomerMore, Consignment
 
-# Create your models here.
+
+class Waybill(models.Model):
+    driver = models.ForeignKey(DriverMore, on_delete=models.SET_NULL, related_name='waybill', null=True)
+    sender = models.ForeignKey(CustomerMore, on_delete=models.SET_NULL, related_name='waybill', null=True)
+    consignment = models.ForeignKey(Consignment, on_delete=models.SET_NULL, related_name='waybill', null=True)
+    created_on = models.DateTimeField(_('create on'), auto_now_add=True)
+    more = models.JSONField(_('more'))
+
+    def __str__(self):
+        return f'consignment: {self.consignment.__str__()}' \
+               f',driver :{self.driver.__str__()},' \
+               f' sender: {self.sender.__str__()}'
