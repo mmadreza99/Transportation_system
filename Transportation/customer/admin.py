446e43a2bcf9c06f5ced676e2b53e9140689b790
mmadreza <mohammd78t@gmail.com>
2021-12-06 23:03:49 +0330

    init admin

--- Transportation/customer/admin.py
+++ Transportation/customer/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import CustomerMore, Consignment
+
+
+class CustomerAdmin(admin.ModelAdmin):
+    fieldsets = (
+        ('Personal info', {
+            'fields': (
+                'user',
+                'address',
+                'Postal_code'
+            )
+        }),
+    )
+
+    def phone_number(self):
+        return self.user.phone_number
+
+    def is_active(self):
+        return self.user.is_active
+
+    list_display = ('user', phone_number, is_active)
+
+
+class ConsignmentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'package_type', 'sender', 'recipient_name')
+    search_fields = ['package_type', ]
+
+
+admin.site.register(CustomerMore, CustomerAdmin)
+admin.site.register(Consignment, ConsignmentAdmin)
