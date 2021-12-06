02f85f600dcae02d9a6ec2c386add9f976ab0ca4
mmadreza <mohammd78t@gmail.com>
2021-12-06 23:09:38 +0330

    edit models

--- Transportation/drivers/models.py
+++ Transportation/drivers/models.py
@@ -3,32 +3,29 @@ from django.utils.translation import ugettext_lazy as _
 from django.contrib.auth import get_user_model
 
 
-User = get_user_model()
-
-
 class Certificate(models.Model):
     class Type(models.TextChoices):
         P1 = 'P1', 'p1'
         P2 = 'P2', 'p2'
         P3 = 'P3', 'p3'
-    name = models.CharField(_('name'), max_length=20)
-    created_time = models.DateTimeField(_('create time'), auto_now_add=True)
+    id = models.CharField(_('id'), max_length=20, primary_key=True)
+    created_time = models.DateTimeField(_('create time'))
+    validity_date = models.DateTimeField(_('validity date'))
     type = models.CharField(_('type'), max_length=10, choices=Type.choices)
     image = models.ImageField(_('image'), upload_to='Certificate', null=True)
 
     def __str__(self):
-        return self.name
+        return str(self.id)
 
 
 class KartHoshmand(models.Model):
-    id = models.IntegerField(primary_key=True)
-    name = models.CharField(_('name'), max_length=50)
-    validity_date = models.DateTimeField(_('validity date'))
-    create_time = models.DateTimeField(_('create time'))
+    id = models.CharField(_('id'), max_length=20, primary_key=True)
+    validity_date = models.DateField(_('validity date'))
+    create_time = models.DateField(_('create time'))
     image = models.ImageField(_('image'), upload_to='kart_hoshmand', null=True)
 
     def __str__(self):
-        return self.id
+        return str(self.id)
 
 
 class Truck(models.Model):
@@ -49,17 +46,18 @@ class Truck(models.Model):
         return f'{self.type}, {self.registration_plate}'
 
 
-class DriverMore(User):
-    user = models.OneToOneField(User, related_name='D_user', on_delete=models.CASCADE)
+class DriverMore(models.Model):
+    user = models.OneToOneField(get_user_model(), related_name='D_user', on_delete=models.CASCADE)
     Date_of_birth = models.DateTimeField(_('birth'), null=True)
     place_of_birth = models.CharField(_('place of birth'), max_length=50, blank=True)
     Driver_licence = models.ForeignKey(Certificate, related_name='Driver', on_delete=models.CASCADE)
     address = models.TextField(_('address'), max_length=150, blank=True)
     kart_hoshmand = models.OneToOneField(KartHoshmand, on_delete=models.CASCADE)
     truck = models.OneToOneField(Truck, on_delete=models.CASCADE)
+    # TODO ADD PHONE NUMBER
 
     def __str__(self):
-        return f'{self.user}, {self.phone_number.as_e164 }'
+        return f'{self.user} phone number: {self.user.phone_number}'
 
     class Meta:
         verbose_name = _('DriverUser')
