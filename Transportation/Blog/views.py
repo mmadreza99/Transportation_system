087311814935bdf9cdb80a342cc22e313849cc1d
mmadreza <mohammd78t@gmail.com>
2021-12-11 00:53:36 +0330

    repath templetes blog

--- Transportation/Blog/views.py
+++ Transportation/Blog/views.py
@@ -7,11 +7,11 @@ from django.shortcuts import render, get_object_or_404
 
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1).order_by('-created_on')
-    template_name = 'index.html'
+    template_name = 'blog/index.html'
 
 
 def post_detail(request, slug):
-    template_name = 'post_detail.html'
+    template_name = 'blog/post_detail.html'
     post = get_object_or_404(Post, slug=slug)
     comments = post.comments.filter(active=True)
     new_comment = None
