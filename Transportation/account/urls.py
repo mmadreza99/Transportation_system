74a009d12ae9b3724e8bd249569eac3114ef4226
mmadreza99 <mohammd78t@gmail.com>
2021-12-22 21:47:35 +0330

    cd file in to app blog

--- Transportation/account/urls.py
+++ Transportation/account/urls.py
@@ -3,7 +3,5 @@ from . import views
 
 
 urlpatterns = [
-    path("", views.home_page_account, name="home_page_account"),
-    path("register/", views.register, name="register"),
-    path("login/", views.login_page, name="login"),
+
 ]
