09cde2aa163fc427b9d26ec9c7c5fd2556f1d6e0
mmadreza99 <mohammd78t@gmail.com>
2021-12-22 21:52:16 +0330

    adding all type user

--- Transportation/account/admin.py
+++ Transportation/account/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import User
+from .models import DriverUser, CustomerUser, AuthorUser
 
 
 class UserAdmin(admin.ModelAdmin):
@@ -45,4 +45,6 @@ class UserAdmin(admin.ModelAdmin):
     )
 
 
-admin.site.register(User, UserAdmin)
+admin.site.register(DriverUser, UserAdmin)
+admin.site.register(CustomerUser, UserAdmin)
+admin.site.register(AuthorUser, UserAdmin)
