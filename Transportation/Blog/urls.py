7c4e05194c39676e8ca181ffa82731ea57d85c7b
mmadreza99 <mohammd78t@gmail.com>
2022-01-03 22:09:33 +0330

    change name urlpatterns

--- Transportation/Blog/urls.py
+++ Transportation/Blog/urls.py
@@ -2,9 +2,9 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.PostList.as_view(), name='home'),
-    path("home", views.home_page_blog, name="home_page_blog"),
-    path("register/", views.register, name="register"),
-    path("login/", views.login_page, name="login"),
+    path('', views.PostList.as_view(), name='blog-home'),
+    path("login-or-register", views.home_page_blog, name="blog_login_or_register"),
+    path("register/", views.register, name="blog-register"),
+    path("login/", views.login_page, name="blog-login"),
     path('<slug:slug>/', views.post_detail, name='post_detail'),
 ]
