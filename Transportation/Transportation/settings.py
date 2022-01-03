a802892ac7b00fcd5d0503bdc84f0af61b0cd596
mmadreza99 <mohammd78t@gmail.com>
2022-01-03 22:13:43 +0330

    adding TIME_ZONE tehran and chang database to postgresql

--- Transportation/Transportation/settings.py
+++ Transportation/Transportation/settings.py
@@ -45,9 +45,15 @@ INSTALLED_APPS = [
     'Blog.apps.BlogConfig',
     'waybill.apps.WaybillConfig',
     'crispy_forms',
+    'debug_toolbar',
+]
+
+INTERNAL_IPS = [
+    '127.0.0.1',
 ]
 
 MIDDLEWARE = [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -81,22 +87,23 @@ WSGI_APPLICATION = 'Transportation.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+DATABASES = {
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql',
+       'NAME': DB['name_db'],
+       'PASSWORD': DB['password_db'],
+       'USER': 'postgres',
+       'PORT': 5432
+   }
+}
+
 # DATABASES = {
 #    'default': {
-#        'ENGINE': 'django.db.backends.postgresql',
-#        'NAME': DB['name_db'],
-#        'PASSWORD': DB['password_db'],
-#        'USER': 'postgres',
-#        'PORT': 5432
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'sqlite3.db'),
 #    }
 # }
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'sqlite3.db'),
-    }
-}
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
@@ -123,7 +130,7 @@ AUTH_USER_MODEL = 'account.User'
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Tehran'
 
 USE_I18N = True
 
