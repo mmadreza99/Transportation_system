d6bda74be18731656c26927129b269faf7bdf38b
mmadreza99 <mohammd78t@gmail.com>
2022-01-03 22:11:52 +0330

    adding urls customer app and __debug__

--- Transportation/Transportation/urls.py
+++ Transportation/Transportation/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import debug_toolbar
 from django.contrib import admin
 from django.urls import path, include
 
@@ -20,5 +21,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', include('Blog.urls')),
     path('account/', include('account.urls')),
-    path('customer/', include('customer.urls'))
+    path('customer/', include('customer.urls')),
+    path('__debug__/', include(debug_toolbar.urls))
 ]
