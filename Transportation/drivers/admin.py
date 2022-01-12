817a631abdc2d5935154a841d2897129c55670b2
mmadreza99 <mohammd78t@gmail.com>
2022-01-12 14:59:06 +0330

    change models to StackedInline

--- Transportation/drivers/admin.py
+++ Transportation/drivers/admin.py
@@ -2,13 +2,21 @@ from django.contrib import admin
 from .models import Truck, DriverMore, Certificate, KartHoshmand
 
 
-class TruckAdmin(admin.ModelAdmin):
-    list_display = ('type', 'id_insurance', 'registration_plate')
-    list_filter = ('type',)
+class TruckInLine(admin.StackedInline):
+    model = Truck
 
 
+class CertificateInLine(admin.StackedInline):
+    model = Certificate
+
+
+class KartHoshmandInLine(admin.StackedInline):
+    model = KartHoshmand
+
+
+@admin.register(DriverMore)
 class DriverAdmin(admin.ModelAdmin):
-    list_display = ('user', 'truck', 'Driver_licence')
+    list_display = ('user',)
 
     fieldsets = (
         ('Personal info', {
@@ -16,25 +24,13 @@ class DriverAdmin(admin.ModelAdmin):
                 'user',
                 'Date_of_birth',
                 'place_of_birth',
-                'truck',
-                'Driver_licence',
-                'kart_hoshmand',
                 'address'
             )
         }),
     )
 
-
-class CertificateAdmin(admin.ModelAdmin):
-    list_display = ('id', 'type', 'created_time')
-    list_filter = ('type',)
+    inlines = [TruckInLine, CertificateInLine, KartHoshmandInLine]
 
 
-class KartHoshmandAdmin(admin.ModelAdmin):
-    list_display = ('id', 'validity_date', 'create_time')
 
 
-admin.site.register(Truck, TruckAdmin)
-admin.site.register(DriverMore, DriverAdmin)
-admin.site.register(Certificate, CertificateAdmin)
-admin.site.register(KartHoshmand, KartHoshmandAdmin)
